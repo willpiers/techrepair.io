194c1d7b751028d56e97e92d487f681e759a68d7
Will Piers <wpiers@rallydev.com>
2015-02-21 23:44:23 -0700

    clean up some styles - turn links blue

--- components/home/home.react.js
+++ components/home/home.react.js
@@ -4,9 +4,9 @@ var Link = require('react-router').Link;
 var Home = React.createClass({
   render: function() {
     return (
-      <div className="home">
+      <div className="home row">
         <p>Welcome to CellTech Repairs - an independently managed tech-repair business!</p>
-        <p>Feel free to check out common repair types/prices, or <Link to='about'>contact me</Link> directly for a more personalized quote.</p>
+        <p>Feel free to check out common <Link to='pricing'>repair types/prices</Link>, or <Link to='about'>contact me</Link> directly for a more personalized quote.</p>
         <p>Thanks for stopping by!</p>
       </div>
     );
